08ec83bc1f344421d6519614cc8fc7e8a29518cc
Langbakke <58025875+Langbakke@users.noreply.github.com>
2022-03-09 15:43:43 +0100

    Accelerometer and Gyroscope data now shown on screen

--- wearable/src/main/java/com/example/wearosapp/MainActivity.kt
+++ wearable/src/main/java/com/example/wearosapp/MainActivity.kt
@@ -1,69 +1,52 @@
 package com.example.wearosapp
 
 import android.app.Activity
-import android.content.BroadcastReceiver
 import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.os.Message
 import android.widget.TextView
-import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.example.wearosapp.databinding.ActivityMainBinding
+import java.io.File
+import java.sql.Timestamp
+import java.util.*
+import kotlin.time.Duration.Companion.seconds
 
-var messageOutCounter : Int ?= null
+
 class MainActivity : Activity(), SensorEventListener {
     var myAccelerometer : Sensor ?= null
     var myGyroScope : Sensor ?= null
     var mySensorManager : SensorManager ?= null
-    var mainHandler : Handler ?= null
-
-    private lateinit var binding: ActivityMainBinding
-    var intentFilter : IntentFilter ?= null
+    var file : File ?= null
+    var startTime : Long ?= null
+    private lateinit var binding : ActivityMainBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
         mySensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
         myGyroScope = mySensorManager!!.getDefaultSensor(Sensor.TYPE_GYROSCOPE)
         myAccelerometer = mySensorManager!!.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
-
-        val myHandler = Handler(Looper.getMainLooper()) { msg ->
-            val stuff = msg.data
-            messageText(stuff.getString("messageText"))
-            true
-        }
-        val messageFilter = IntentFilter(Intent.ACTION_SEND)
-        val messageReceiver = Receiver()
-        LocalBroadcastManager.getInstance(this).registerReceiver(messageReceiver, messageFilter)
-    }
-    class Receiver(textView: TextView, messageInCounter : Int) : BroadcastReceiver(){
-        override fun onReceive(p0: Context?, p1: Intent?) {
-            val message = "I just Received a message" + (messageInCounter)
-
-        }
-
-
-    }
-    fun messageText(newInfo: String?) {
-        if (newInfo!!.compareTo("") != 0)
-            textView!!.append("\n" + newInfo)
+        file = File.createTempFile("SensorData", ".txt", null)
+        file.run { this!!.setWritable(true) }
     }
 
     override fun onResume() {
         super.onResume()
-        mySensorManager!!.registerListener(this, myAccelerometer, SensorManager.SENSOR_DELAY_NORMAL)
-        mySensorManager!!.registerListener(this, myGyroScope, SensorManager.SENSOR_DELAY_NORMAL)
+        val time = Date()
+        startTime = time.time.seconds.inWholeSeconds
+        mySensorManager!!.registerListener(this, myAccelerometer, SensorManager.SENSOR_DELAY_UI)
+        mySensorManager!!.registerListener(this, myGyroScope, SensorManager.SENSOR_DELAY_UI)
     }
+
+    private fun WriteToFile(accelx : Float, accely: Float, accelz:Float, file : File){
+
+    }
+
 
     override fun onPause() {
         super.onPause()
@@ -73,19 +56,24 @@ class MainActivity : Activity(), SensorEventListener {
         val accelx = findViewById<TextView>(R.id.accel_val_x)
         val accely = findViewById<TextView>(R.id.accel_val_y)
         val accelz = findViewById<TextView>(R.id.accel_val_z)
+
         val gyrox = findViewById<TextView>(R.id.gyro_val_x)
         val gyroy = findViewById<TextView>(R.id.gyro_val_y)
         val gyroz = findViewById<TextView>(R.id.gyro_val_z)
-        if ((p0 != null) && (p0.sensor.type == Sensor.TYPE_ACCELEROMETER)) {
+        val currentTime = Date()
+        if ((currentTime.time.seconds.inWholeSeconds.toInt() % 60) == 0) {
+            if ((p0 != null) && (p0.sensor.type == Sensor.TYPE_ACCELEROMETER)) {
+                accelx.text = p0.values[0].toString()
+                accely.text = p0.values[1].toString()
+                accelz.text = p0.values[2].toString()
 
-            accelx.text = p0.values[0].toString()
-            accely.text = p0.values[1].toString()
-            accelz.text = p0.values[2].toString()
+            }else if((p0 != null) && (p0.sensor.type == Sensor.TYPE_GYROSCOPE)){
+                gyrox.text = p0.values[0].toString()
+                gyroy.text = p0.values[1].toString()
+                gyroz.text = p0.values[2].toString()
 
-        }else if((p0 != null) && (p0.sensor.type == Sensor.TYPE_GYROSCOPE)){
-            gyrox.text = p0.values[0].toString()
-            gyroy.text = p0.values[1].toString()
-            gyroz.text = p0.values[2].toString()
+            }
+
         }
     }
 

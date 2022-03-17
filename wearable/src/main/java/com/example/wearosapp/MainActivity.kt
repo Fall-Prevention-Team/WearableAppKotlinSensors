344ec7bea39b8db51d5546ac3f82e8a1fe2531be
kristnaKris <56825435+kristnaKris@users.noreply.github.com>
2022-03-17 10:23:53 +0100

    New setup

---
+++ wearable/src/main/java/com/example/wearosapp/MainActivity.kt
@@ -0,0 +1,118 @@
+package com.example.wearosapp
+
+import android.Manifest
+import android.Manifest.permission.BLUETOOTH_CONNECT
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothServerSocket
+import android.bluetooth.BluetoothSocket
+import android.content.ContentValues.TAG
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import android.widget.TextView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.PermissionChecker.PERMISSION_DENIED
+import com.example.wearosapp.databinding.ActivityMainBinding
+import java.io.File
+import java.io.*
+import java.nio.ByteBuffer
+import java.sql.Timestamp
+import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.time.Duration.Companion.seconds
+
+
+class MainActivity : Activity(), SensorEventListener {
+    var myAccelerometer : Sensor ?= null
+    var myGyroScope : Sensor ?= null
+    var mySensorManager : SensorManager ?= null
+    var file : File ?= null
+    var startTime : Long ?= null
+    private lateinit var binding : ActivityMainBinding
+    var SensorData = ArrayList<String>()
+    var uuid: UUID = UUID.fromString("8989063a-c9af-463a-b3f1-f21d9b2b827b")
+    var btCntService : BluetoothConnectionService ?= null
+    var bytes : ByteArray = ByteArray(1024)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        if (ContextCompat.checkSelfPermission(applicationContext, BLUETOOTH_CONNECT)
+//            == PERMISSION_DENIED) {
+//            ActivityCompat.requestPermissions(
+//                this, arrayOf(BLUETOOTH_CONNECT), 1
+//            )
+//        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_main)
+        btCntService = BluetoothConnectionService(applicationContext)
+        mySensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        myGyroScope = mySensorManager!!.getDefaultSensor(Sensor.TYPE_GYROSCOPE)
+        myAccelerometer = mySensorManager!!.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        btCntService?.startService()
+        /*file = File.createTempFile("SensorData", ".txt", null)*/
+        /*file!!.setWritable(true)*/
+        /*val time = Date()
+        startTime = time.time.seconds.inWholeSeconds*/
+        mySensorManager!!.registerListener(this, myAccelerometer, SensorManager.SENSOR_DELAY_UI)
+        mySensorManager!!.registerListener(this, myGyroScope, SensorManager.SENSOR_DELAY_UI)
+    }
+
+
+    override fun onPause() {
+        super.onPause()
+        mySensorManager!!.unregisterListener(this)
+
+    }
+    override fun onSensorChanged(p0: SensorEvent?) {
+        val accelx = findViewById<TextView>(R.id.accel_val_x)
+        val accely = findViewById<TextView>(R.id.accel_val_y)
+        val accelz = findViewById<TextView>(R.id.accel_val_z)
+
+        val gyrox = findViewById<TextView>(R.id.gyro_val_x)
+        val gyroy = findViewById<TextView>(R.id.gyro_val_y)
+        val gyroz = findViewById<TextView>(R.id.gyro_val_z)
+        val currentTime = Date()
+
+
+        if ((currentTime.time.seconds.inWholeSeconds.toInt() % 5) == 0) {
+            if ((p0 != null) && (p0.sensor.type == Sensor.TYPE_ACCELEROMETER)) {
+                accelx.text = p0.values[0].toString()
+                accely.text = p0.values[1].toString()
+                accelz.text = p0.values[2].toString()
+                SensorData.add(p0.values.toString())
+                for (i in 0..bytes.size){
+                    bytes[i] = SensorData[i].toByte()
+                }
+                btCntService?.write(bytes)
+        /*        fileWriter.append(p0.values.toString(), 0, 2)*/
+
+            }else if((p0 != null) && (p0.sensor.type == Sensor.TYPE_GYROSCOPE)){
+                gyrox.text = p0.values[0].toString()
+                gyroy.text = p0.values[1].toString()
+                gyroz.text = p0.values[2].toString()
+                /*fileWriter.append(p0.values.toString(), 0, 2)*/
+            }
+        }
+    }
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+        return
+    }
+
+}

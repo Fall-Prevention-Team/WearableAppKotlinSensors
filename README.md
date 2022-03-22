98c69445b16aadef1f6563be465394efd0b8f7e1
kristnaKris <56825435+kristnaKris@users.noreply.github.com>
2022-03-22 10:19:12 +0100

    Transfer data with message client

---
+++ README.md
@@ -0,0 +1,63 @@
+# Message communication using Wearable Data Layer - Android/Wear OS
+ 
+A two way message communication app for Wear OS that makes use of Wearable data layer. Send/receive messages from your wearable/mobile device.
+
+&nbsp;
+### Usage:
+1. Open project and sync gradle.
+2. Modify the code to suit your needs.
+3. Watch out for more features.
+
+
+### To send/receive images:
+1. Install the app on your wearable and mobile device.
+2. Important - Make sure your watch is paired with your mobile device through bluetooth. You can pair your watch with your phone through the 'Wear OS' app.
+3. Once the device is paired, tap the 'Check for connected wearables' button in the mobile app.
+4. Finally type your message and send the message to your wearable device. You can follow the same to send a message from your wearable device to your mobile device. 
+
+&nbsp;
+### Features :
+- Send/receive messages from your wearable/mobile device(Two way message communication).
+- Shows the full log of the message history.
+- Message acknowledgment and timeout.
+- Supports both round and square Wear OS devices
+
+&nbsp;
+### Preview : 
+![Preview](https://github.com/BharathVishal/Message-communication-using-Wearable-Data-Layer/blob/master/Preview/PreviewGif.gif)
+
+
+&nbsp;
+### Screenshots : 
+![Screenshot 2](https://github.com/BharathVishal/Message-communication-using-Wearable-Data-Layer/blob/master/Screenshots/2.png?s=10)
+![Screenshot 1](https://github.com/BharathVishal/Message-communication-using-Wearable-Data-Layer/blob/master/Screenshots/1.png?s=10)
+
+
+
+&nbsp;
+### Year developed : 
+2019
+
+
+&nbsp;
+### SDK Info : 
+Mobile version
+Min SDK : 23  | Target SDK : 32 | Gradle : 7.1.2  | Kotlin 
+
+Wearable version
+Min SDK : 25  | Target SDK : 32 | Gradle : 7.1.2  | Kotlin 
+
+&nbsp;
+### Device Requirements : 
+Wear OS 2.x
+
+
+&nbsp;
+### Android Studio Version : 
+Android Studio Bumblebee | 2021.1.1 Patch 2
+
+
+&nbsp;
+© 2019-2022. Developed by Bharath Vishal G (https://github.com/BharathVishal).
+
+Thank you. :smile:
---
+++ wear/src/main/java/com/bharathvishal/messagecommunicationusingwearabledatalayer/MainActivity.kt
@@ -0,0 +1,296 @@
+package com.bharathvishal.messagecommunicationusingwearabledatalayer
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ScrollView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.wear.ambient.AmbientModeSupport
+import androidx.wear.ambient.AmbientModeSupport.AmbientCallback
+import com.bharathvishal.messagecommunicationusingwearabledatalayer.databinding.ActivityMainBinding
+import com.google.android.gms.wearable.*
+import java.nio.charset.StandardCharsets
+
+class MainActivity : AppCompatActivity(), AmbientModeSupport.AmbientCallbackProvider,
+    DataClient.OnDataChangedListener,
+    MessageClient.OnMessageReceivedListener,
+    CapabilityClient.OnCapabilityChangedListener {
+    private var activityContext: Context? = null
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val TAG_MESSAGE_RECEIVED = "receive1"
+    private val APP_OPEN_WEARABLE_PAYLOAD_PATH = "/APP_OPEN_WEARABLE_PAYLOAD"
+
+    private var mobileDeviceConnected: Boolean = false
+
+
+    // Payload string items
+    private val wearableAppCheckPayloadReturnACK = "AppOpenWearableACK"
+
+    private val MESSAGE_ITEM_RECEIVED_PATH: String = "/message-item-received"
+
+
+    private var messageEvent: MessageEvent? = null
+    private var mobileNodeUri: String? = null
+
+    private lateinit var ambientController: AmbientModeSupport.AmbientController
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        activityContext = this
+
+        // Enables Always-on
+        ambientController = AmbientModeSupport.attach(this)
+
+
+        //On click listener for sendmessage button
+//        binding.sendmessageButton.setOnClickListener {
+//            if (mobileDeviceConnected) {
+//                if (binding.messagecontentEditText.text!!.isNotEmpty()) {
+//
+//                    val nodeId: String = messageEvent?.sourceNodeId!!
+//                    // Set the data of the message to be the bytes of the Uri.
+//                    val payload: ByteArray =
+//                        binding.messagecontentEditText.text.toString().toByteArray()
+//
+//                    // Send the rpc
+//                    // Instantiates clients without member variables, as clients are inexpensive to
+//                    // create. (They are cached and shared between GoogleApi instances.)
+//                    val sendMessageTask =
+//                        Wearable.getMessageClient(activityContext!!)
+//                            .sendMessage(nodeId, MESSAGE_ITEM_RECEIVED_PATH, payload)
+//
+//                    binding.deviceconnectionStatusTv.visibility = View.GONE
+//
+//                    sendMessageTask.addOnCompleteListener {
+//                        if (it.isSuccessful) {
+//                            Log.d("send1", "Message sent successfully")
+//                            val sbTemp = StringBuilder()
+//                            sbTemp.append("\n")
+//                            sbTemp.append(binding.messagecontentEditText.text.toString())
+//                            sbTemp.append(" (Sent to mobile)")
+//                            Log.d("receive1", " $sbTemp")
+//                            binding.messagelogTextView.append(sbTemp)
+//
+//                            binding.scrollviewTextMessageLog.requestFocus()
+//                            binding.scrollviewTextMessageLog.post {
+//                                binding.scrollviewTextMessageLog.fullScroll(ScrollView.FOCUS_DOWN)
+//                            }
+//                        } else {
+//                            Log.d("send1", "Message failed.")
+//                        }
+//                    }
+//                } else {
+//                    Toast.makeText(
+//                        activityContext,
+//                        "Data object is empty.",
+//                        Toast.LENGTH_SHORT
+//                    ).show()
+//                }
+//            }
+//        }
+    }
+
+
+    private fun sendData(data:ByteArray){
+        if (mobileDeviceConnected) {
+            if (data.isNotEmpty()) {
+                val nodeId: String = messageEvent?.sourceNodeId!!
+                // Set the data of the message to be the bytes of the Uri.
+                val payload: ByteArray = data
+                // Send the rpc
+                // Instantiates clients without member variables, as clients are inexpensive to
+                // create. (They are cached and shared between GoogleApi instances.)
+                val sendMessageTask =
+                    Wearable.getMessageClient(activityContext!!)
+                        .sendMessage(nodeId, MESSAGE_ITEM_RECEIVED_PATH, payload)
+
+                binding.deviceconnectionStatusTv.visibility = View.GONE
+
+                sendMessageTask.addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        Log.d("send1", "Message sent successfully")
+                        val sbTemp = StringBuilder()
+                        sbTemp.append("\n")
+                        sbTemp.append(binding.messagecontentEditText.text.toString())
+                        sbTemp.append(" (Sent to mobile)")
+                        Log.d("receive1", " $sbTemp")
+                        binding.messagelogTextView.append(sbTemp)
+
+                        binding.scrollviewTextMessageLog.requestFocus()
+                        binding.scrollviewTextMessageLog.post {
+                            binding.scrollviewTextMessageLog.fullScroll(ScrollView.FOCUS_DOWN)
+                        }
+                    } else {
+                        Log.d("send1", "Message failed.")
+                    }
+                }
+            } else {
+                Toast.makeText(
+                    activityContext,
+                    "Data object is empty.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
+    override fun onDataChanged(p0: DataEventBuffer) {
+    }
+
+    override fun onCapabilityChanged(p0: CapabilityInfo) {
+    }
+
+
+    @SuppressLint("SetTextI18n")
+    override fun onMessageReceived(p0: MessageEvent) {
+        try {
+            Log.d(TAG_MESSAGE_RECEIVED, "onMessageReceived event received")
+            val s1 = String(p0.data, StandardCharsets.UTF_8)
+            val messageEventPath: String = p0.path
+
+            Log.d(
+                TAG_MESSAGE_RECEIVED,
+                "onMessageReceived() A message from watch was received:"
+                        + p0.requestId
+                        + " "
+                        + messageEventPath
+                        + " "
+                        + s1
+            )
+
+            //Send back a message back to the source node
+            //This acknowledges that the receiver activity is open
+            if (messageEventPath.isNotEmpty() && messageEventPath == APP_OPEN_WEARABLE_PAYLOAD_PATH) {
+                try {
+                    // Get the node id of the node that created the data item from the host portion of
+                    // the uri.
+                    val nodeId: String = p0.sourceNodeId.toString()
+                    // Set the data of the message to be the bytes of the Uri.
+                    val returnPayloadAck = wearableAppCheckPayloadReturnACK
+                    val payload: ByteArray = returnPayloadAck.toByteArray()
+
+                    // Send the rpc
+                    // Instantiates clients without member variables, as clients are inexpensive to
+                    // create. (They are cached and shared between GoogleApi instances.)
+                    val sendMessageTask =
+                        Wearable.getMessageClient(activityContext!!)
+                            .sendMessage(nodeId, APP_OPEN_WEARABLE_PAYLOAD_PATH, payload)
+
+                    Log.d(
+                        TAG_MESSAGE_RECEIVED,
+                        "Acknowledgement message successfully with payload : $returnPayloadAck"
+                    )
+
+                    messageEvent = p0
+                    mobileNodeUri = p0.sourceNodeId
+
+                    sendMessageTask.addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            Log.d(TAG_MESSAGE_RECEIVED, "Message sent successfully")
+                            binding.messagelogTextView.visibility = View.VISIBLE
+
+                            val sbTemp = StringBuilder()
+                            sbTemp.append("\nMobile device connected.")
+                            Log.d("receive1", " $sbTemp")
+                            binding.messagelogTextView.append(sbTemp)
+
+                            mobileDeviceConnected = true
+
+                            binding.textInputLayout.visibility = View.VISIBLE
+                            binding.sendmessageButton.visibility = View.VISIBLE
+                            binding.deviceconnectionStatusTv.visibility = View.VISIBLE
+                            binding.deviceconnectionStatusTv.text = "Mobile device is connected"
+                        } else {
+                            Log.d(TAG_MESSAGE_RECEIVED, "Message failed.")
+                        }
+                    }
+                } catch (e: Exception) {
+                    Log.d(
+                        TAG_MESSAGE_RECEIVED,
+                        "Handled in sending message back to the sending node"
+                    )
+                    e.printStackTrace()
+                }
+            }//emd of if
+            else if (messageEventPath.isNotEmpty() && messageEventPath == MESSAGE_ITEM_RECEIVED_PATH) {
+                try {
+                    binding.messagelogTextView.visibility = View.VISIBLE
+                    binding.textInputLayout.visibility = View.VISIBLE
+                    binding.sendmessageButton.visibility = View.VISIBLE
+                    binding.deviceconnectionStatusTv.visibility = View.GONE
+
+                    val sbTemp = StringBuilder()
+                    sbTemp.append("\n")
+                    sbTemp.append(s1)
+                    sbTemp.append(" - (Received from mobile)")
+                    Log.d("receive1", " $sbTemp")
+                    binding.messagelogTextView.append(sbTemp)
+
+
+                    binding.scrollviewTextMessageLog.requestFocus()
+                    binding.scrollviewTextMessageLog.post {
+                        binding.scrollviewTextMessageLog.fullScroll(ScrollView.FOCUS_DOWN)
+                    }
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        } catch (e: Exception) {
+            Log.d(TAG_MESSAGE_RECEIVED, "Handled in onMessageReceived")
+            e.printStackTrace()
+        }
+    }
+
+
+    override fun onPause() {
+        super.onPause()
+        try {
+            Wearable.getDataClient(activityContext!!).removeListener(this)
+            Wearable.getMessageClient(activityContext!!).removeListener(this)
+            Wearable.getCapabilityClient(activityContext!!).removeListener(this)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        try {
+            Wearable.getDataClient(activityContext!!).addListener(this)
+            Wearable.getMessageClient(activityContext!!).addListener(this)
+            Wearable.getCapabilityClient(activityContext!!)
+                .addListener(this, Uri.parse("wear://"), CapabilityClient.FILTER_REACHABLE)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    override fun getAmbientCallback(): AmbientCallback = MyAmbientCallback()
+
+    private inner class MyAmbientCallback : AmbientCallback() {
+        override fun onEnterAmbient(ambientDetails: Bundle) {
+            super.onEnterAmbient(ambientDetails)
+        }
+
+        override fun onUpdateAmbient() {
+            super.onUpdateAmbient()
+        }
+
+        override fun onExitAmbient() {
+            super.onExitAmbient()
+        }
+    }
+
+}
